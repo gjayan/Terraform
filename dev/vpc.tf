7e9796511ccf1bc9706de22aa3e29d2747eb4bb1
gjayan <119437626+gjayan@users.noreply.github.com>
2023-10-27 20:44:49 +0530

    Create vpc.tf

---
+++ dev/vpc.tf
@@ -0,0 +1,31 @@
+terraform {
+        required_providers {
+          aws = {
+                source  = "hashicorp/aws"
+                version = ">= 2.7.0"
+          }
+        }
+  }
+
+provider "vault" {
+  address = "http://13.232.111.8:8200"
+  auth_login_userpass {
+    username = "gjayan"
+    }
+}
+
+data "vault_generic_secret" "env" {
+  path = "kv-terraform/dev"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block       = vault_generic_secret.env.data["ip"]
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Gayathri-demo-vpc"
+    Used_By = "Gayathri"
+
+  }
+}
+
