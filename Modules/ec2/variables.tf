386cb8580f55e8f6b919526a40b066d26908a1f8
gjayan <119437626+gjayan@users.noreply.github.com>
2024-03-10 19:06:32 +0530

    Add files via upload

---
+++ Modules/ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami-id" {
+    type = string
+}
+
+variable "instance-type" {
+    type = string
+}
+
+variable "security-group" {
+    type = string
+}
+
+variable "tags" {
+    type = any
+}
