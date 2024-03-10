040d30bcf40f1a29a60a4c5b934ce43335dea5f5
gjayan <119437626+gjayan@users.noreply.github.com>
2024-03-10 19:07:46 +0530

    Create main.tf

---
+++ Modules/security-group/main.tf
@@ -0,0 +1,58 @@
+(* data "aws_vpc" "default" {
+  id = var.vpc-id
+//  filter {
+//     name = "tag:Name"
+//     value = [""]
+//  }
+} *)
+
+resource "aws_security_group" "security-group" {
+  name        = var.sg-name
+  description = var.sg-description
+  vpc_id      = data.aws_vpc.default.id
+
+  dynamic "ingress" {
+    for_each = var.ingress-rules
+    content {
+    description      = lookup(ingress.value, "description", "Ingress rule for (${ingress.value.protocol}) protocol")
+    from_port        = lookup(ingress.value, "from-port")
+    to_port          = lookup(ingress.value, "to-port")
+    protocol         = lookup(ingress.value, "protocol")
+    cidr_blocks      = [data.aws_vpc.default.cidr_block]
+            }
+                    }
+
+
+  dynamic "egress" {
+    for_each = var.egress-rules
+    content {
+    from_port        = lookup(egress.value, "from-port")
+    to_port          = lookup(egress.value, "to-port")
+    protocol         = lookup(egress.value, "protocol")
+    cidr_blocks      = [lookup(egress.value, "cidr-block")]
+           }
+                   }
+
+
+                   
+
+(* //   dynamic "tag" {
+//    for_each = var.tags
+//    content {
+//    key = tag.key
+//    value =  tag.value
+//   }
+// }
+ *)
+tags = var.tags
+ 
+lifecycle {
+  precondition {
+    condition = contains(["Name", "Used_by"], var.key)
+    error_message = "tags[\"Used_by and Name\"] must be \"present\"."
+  }
+}
+}
+
+
+#tags, validation, main main.tf, try separately, add vpc-id in data
